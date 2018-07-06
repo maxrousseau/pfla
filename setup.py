133fbf23d7601fcb32feee0915275a4dfa97340d
maxime <max@tpd-x220>
2018-07-06 17:43:23 -0400

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='pfla',
-      version='0.1.0',
+      version='0.1.1',
       description='Python facial landmarking and analysis',
       long_description=readme(),
       classifiers=[
