a94373be0b7e8b187e094a8e63259576e3cb1d8d
maxrousseau <maximerousseau08@gmail.com>
2020-05-08 16:45:49 -0400

    PyPI uploaded ready for new github release

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Publication
 This software was published in
 [JOSS](https://joss.theoj.org/papers/10.21105/joss.00855). Since version 1.0.0,
 the packaged has changed considerably. The publication release is still
-available [here](https://github.com/maxrousseau/pfla/releases/tag/v0.1.1)
+available [here](https://github.com/maxrousseau/pfla/releases/tag/v0.1.1).
 
 
 Citing
--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
 setup(name='pfla',
       version='1.0.0',
       description='Python facial landmarking and analysis',
-      long_description=readme(),
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       classifiers=['Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
